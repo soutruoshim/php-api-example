32915cb5853bc0821e0e195aa30cb7751b8de0a5
DESKTOP-3AEO532\User <sout.rahim@gmail.com>
2022-03-24 16:53:27 +0700

    update api

--- api/config/core.php
+++ api/config/core.php
@@ -10,4 +10,20 @@
     $issued_at = time();
     $expiration_time = $issued_at + (60 * 60); // valid for 1 hour
     $issuer = "http://localhost/php-api/api/";
+
+    //=========================for simple====================
+    // show error reporting
+    ini_set('display_errors', 1);
+    
+    // home page url
+    $home_url="http://localhost/api/";
+    
+    // page given in URL parameter, default page is one
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    
+    // set number of records per page
+    $records_per_page = 5;
+    
+    // calculate for the query LIMIT clause
+    $from_record_num = ($records_per_page * $page) - $records_per_page;
 ?>
